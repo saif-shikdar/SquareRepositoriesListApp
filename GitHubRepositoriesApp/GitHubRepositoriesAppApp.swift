102eabdddc355a6582488c75232ed1e8535862aa
Saif Shikdar <43826661+saif-shikdar@users.noreply.github.com>
2023-12-06 11:15:57 +0000

    Initial Commit

---
+++ GitHubRepositoriesApp/GitHubRepositoriesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubRepositoriesAppApp.swift
+//  GitHubRepositoriesApp
+//
+//  Created by Saif Shikdar on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GitHubRepositoriesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
